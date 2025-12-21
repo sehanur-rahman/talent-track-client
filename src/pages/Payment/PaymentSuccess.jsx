617fdc1150f1c1f738ba2cee2e133975deb65dab
sehanur24 <sehanurrahmansiam@gmail.com>
2025-12-22 04:25:48 +0600

    twentyth commit

---
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -0,0 +1,36 @@
+import { useLocation, Link } from "react-router-dom";
+import { FaCheckCircle } from "react-icons/fa";
+
+const PaymentSuccess = () => {
+    const { state } = useLocation();
+    const { scholarshipName, amount } = state || {};
+
+    return (
+        <div className="min-h-screen flex items-center justify-center px-4">
+            <div className="bg-white rounded-2xl shadow-xl p-8 text-center max-w-md w-full">
+                <FaCheckCircle className="text-green-500 text-6xl mx-auto mb-4" />
+
+                <h2 className="text-3xl font-bold mb-2">
+                    Payment Successful
+                </h2>
+
+                <p className="font-semibold mt-2">
+                    {scholarshipName}
+                </p>
+
+                <p className="text-lg font-bold mt-3">
+                    Amount Paid: ${amount}
+                </p>
+
+                <Link
+                    to="/dashboard/my-applications"
+                    className="btn btn-primary mt-6 w-full"
+                >
+                    Go to My Applications
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default PaymentSuccess;
